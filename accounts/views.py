e573ff0d2b2a76a8689e94ada895ce4c427f1eee
SUNWOOPARK <psw261@gmail.com>
2019-11-28 06:21:03 +0900

    add signup and make profile

--- accounts/views.py
+++ accounts/views.py
@@ -4,23 +4,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.shortcuts import render
-
+from .forms import SignupForm
 
 '''
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {
         "form": form,
     })
 '''
 
-signup = CreateView.as_view(model=User, form_class=UserCreationForm)
+signup = CreateView.as_view(model=User,
+                            form_class=SignupForm,
+                            success_url=settings.LOGIN_URL,
+                            template_name='accounts/signup.html')
 
 @login_required()
 def profile(request):
