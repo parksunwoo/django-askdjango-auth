2b8c4f172a98807f602e1d519949ad016cabfd66
SUNWOOPARK <psw261@gmail.com>
2019-12-04 23:59:28 +0900

    add email through url login

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name="profile"),
+
+    path('login/url/', views.RequestLoginViaUrlView.as_view(), name='request_login_via_url'),
+    path('login/<uidb64>/<token>/', views.login_via_url, name='login_via_url'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import PasswordResetView
+from django.http import Http404
+from django.utils.http import urlsafe_base64_decode
 from django.views.generic import CreateView
 from django.shortcuts import redirect, render, resolve_url
 from .forms import SignupForm
@@ -42,3 +48,54 @@ signup = SignupView.as_view()
 @login_required()
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+class RequestLoginViaUrlView(PasswordResetView):
+    template_name = 'accounts/request_login_via_url_form.html'
+    title = '이메일을 통한 로그인'
+    email_template_name = 'accounts/login_via_url.html'
+    success_url = settings.LOGIN_URL
+
+def login_via_url(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        current_user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist, ValidationError):
+        raise Http404
+
+    if default_token_generator.check_token(current_user, token):
+        auth_login(request, current_user)
+        messages.info(request, '로그인이 승인되었습니다.')
+        return redirect('root')
+
+    messages.error(request, '로그인이 거부되었습니다.')
+    return redirect('root')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
